587590494338757223a7e51d857518ed2cae2cc1
BhaveshKhanpara <bhavesh561988@gmail.com>
2014-10-10 14:52:13 +0530

    Update example.php

--- example.php
+++ example.php
@@ -1,38 +1,25 @@
 <?php 
 	
 	
-	$conn = mysql_connect('10.2.1.92','rhapsody','rhapsody');
-	$db = mysql_select_db('rhapsodyofrealities',$conn);
+	$conn = mysql_connect('localhost','root','password');
+	$db = mysql_select_db('mytestBD',$conn);
 
 	if(!$conn){
 		echo 'Database is not connected ...Please check all the details';
 	}
 	
 	include_once('csv.inc.php');
+	
 	$csv = new csv_uploder('Normal_12_months.csv', 1000 , ',');
 	$array = $csv->getCsv();
 	
-
-	/*echo '<pre>';
-	print_r($array);
-	exit;*/
 	
-	$sqlimplode = '';
 	foreach ($array as $value) {
-		//$sqlimplode = "('audio','5',1,".$value[0].")";
-
-		//$sqlimplode = rtrim($sqlimplode,',');
-
-	   // $sql_query = "INSERT INTO pins (coupons_for,plan_id,activation_pin) VALUES ('ebook','1',".$value[0].")";
-		//mysql_query($sql_query);
-
-	//	exit;
+		
+		//Bulid your custom querty to insert CSV Data to Database
+		
 	}
 
-	//$sqlimplode = rtrim($sqlimplode,',');
-
-	//echo $sql_query = "INSERT INTO pins (coupons_for,plan_id,activation_pin,campaign_name) VALUES $sqlimplode";
-	//mysql_query($sql_query);
 
 
 ?>
